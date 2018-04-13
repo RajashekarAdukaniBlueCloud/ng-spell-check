5f70efcbf18005be0518be1465c53b7a6a46d1de
Eddie <eddiel@ca.ibm.com>
2018-04-13 14:34:18 -0400

    added textarea

--- src/app/modules/spell-check/spell-check.component.ts
+++ src/app/modules/spell-check/spell-check.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable }  from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'ng-spell-check',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spell-check.component.css']
 })
 export class SpellCheckComponent implements OnInit {
+  public  content = "";
+  private textAreaCtrlSub;
 
-  constructor() { }
+  textAreaCtrl = new FormControl();
 
+  constructor() {
+    this.textAreaCtrlSub = this.textAreaCtrl.valueChanges
+    .debounceTime(3000)
+    .subscribe((value)=>{
+      console.log(value);
+    })
+   }
+  
   ngOnInit() {
   }
 
+  
 }
--- src/app/modules/spell-check/spell-check.module.ts
+++ src/app/modules/spell-check/spell-check.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SpellCheckComponent } from './spell-check.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     SpellCheckComponent
